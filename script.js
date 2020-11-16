10f17197ef2237f9cf40b1ad4c26de99c97d64eb
Filip Kaczmarek <61003790+FilipKaczmarek@users.noreply.github.com>
2020-11-16 17:40:22 +0100

    Upload vs. 1
    HTML and CSS 
    without JS

---
+++ script.js
@@ -0,0 +1,6 @@
+const numberButtons = document.querySelectorAll(`[data-number]`);
+const operationButtons = document.querySelectorAll(`[data-operation]`);
+const equalButton = document.querySelector(`[data-equals]`);
+const deleteButton = document.querySelector(`[data-delete]`);
+const mathematicalOperations = document.querySelector(`.mathematical-operation`);
+const finalResult = document.querySelector(`.final-results`);
